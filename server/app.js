e4e7ffb05dc7ad1912bcbd8f5c4c5d48a6276991
Nilesh yadav <rameshkumaryadav1918@gmail.com>
2025-06-13 18:39:13 +0530

    error fixed

--- server/app.js
+++ server/app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 })
 app.use(cookieParser())
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: "https://personal-storage-driver.vercel.app", credentials: true }))
 
 app.use("/directory", directoryRouter)
 app.use("/file", fileRouter)

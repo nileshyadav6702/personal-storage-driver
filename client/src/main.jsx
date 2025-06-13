35361c4942c758f8581c48eb38ecad424ff2ff39
Nilesh yadav <rameshkumaryadav1918@gmail.com>
2025-06-13 09:59:24 +0530

    changed the routing configuration

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,12 +8,12 @@ import Register from './pages/Register.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   </StrictMode>
 )
